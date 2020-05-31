ada1e880339894890fee77c650eaafa43b8d5cf4
lizihao <zihao.li01@ucarinc.com>
2020-05-31 20:13:09 +0800

    init

--- src/main/java/com/ucar/anno/ApiParam.java
+++ src/main/java/com/ucar/anno/ApiParam.java
@@ -1,11 +1,18 @@
 package com.ucar.anno;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 /**
  * Description:
  *
  * @author 李自豪（zihao.li01@ucarinc.com）
  * @since 2020/5/15
  */
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
 public @interface ApiParam {
 
 }
--- src/main/java/com/ucar/anno/ApiProperty.java
+++ src/main/java/com/ucar/anno/ApiProperty.java
@@ -2,12 +2,19 @@ package com.ucar.anno;
 
 import com.ucar.constant.PropertyTypeEnum;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 /**
  * Description:
  *
  * @author 李自豪（zihao.li01@ucarinc.com）
  * @since 2020/5/15
  */
+@Target({ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
 public @interface ApiProperty {
 
     String value() default "";
